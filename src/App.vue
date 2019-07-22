e11379a86a6dab002026f706e046f743bf80b793
Mario Ochoa <mario8ato@hotmail.com>
2019-07-22 00:36:20 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="container">
+    <Cabezera></Cabezera>
+    <Lista></Lista>
+  </div>
+</template>
+
+<script>
+// import LOQUESEA from '
+import Cabezera from './components/Cabezera.vue'
+import Lista from './components/Lista.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Cabezera,
+    Lista
+  }
+}
+</script>
---
+++ src/components/Cabezera.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>Frutas</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Cabezera'   
+}
+</script>
